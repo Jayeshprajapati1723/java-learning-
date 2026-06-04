d03fd8cd542047676c0fe9be39e06bb52e712bcb
Jayeshprajapati1723 <jayesh.prajapati2024@sait.ac.in>
2026-06-04 22:32:22 +0530

    java first code

---
+++ jayesh.java
@@ -0,0 +1,19 @@
+class jayesh {         
+public static void main(String args[]) {
+
+System.out.println("SUM FIRST PROGRAME");
+int a = 10 ; int b = 20 ;
+int c = a+b ;
+System.out.println("sum of a+b = " +c );
+
+
+}
+}
+
+//javac filename.java // java filename without extension compil // e and execute 
+// comments are the statementes that are ignored by the compiler 
+// comments are used to increase readablity of our code 
+
+/*
+multiple line comments 
+*/
