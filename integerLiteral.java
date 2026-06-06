675c52a72dfaf4dfef8ecfad9e0b74f3955388fc
Jayeshprajapati1723 <jayesh.prajapati2024@sait.ac.in>
2026-06-06 19:25:57 +0530

    add gitignoe and java files

---
+++ integerLiteral.java
@@ -0,0 +1,33 @@
+ public class integerLiteral {
+    public static void main (String arg[]) {
+// jo class public hogi vo hi run hogi phle kitni bhi class ho public class se hi run hoga phle 
+
+// ek hi class public ho skti h rule one 
+// public  class name or file name same ho 
+
+// identifiying rulee a-z 0-9 any symbol currency 
+// only start with letters not with numerical 
+
+int a = 104 ;
+int b = 0401 ;
+int c = 0550000 ; 
+int d = 01000 ;
+
+System.out.println(a) ;
+System.out.println(b) ;
+System.out.println(c) ;
+System.out.println(d) ;
+
+int n = 2___5 ;//kr skte h readablity k liye
+// int nm = ____5____________6 ;nhi kr skte h 
+    }
+}
+
+// output 
+/*C:\Users\jayesh prajapati\OneDrive\Desktop\java  codes\java-learning->java integerLiteral
+104
+257
+184320
+512 */
+
+
