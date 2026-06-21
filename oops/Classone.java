37db3f2040d628462b9b0adfd20650f46ffa82c0
Jayeshprajapati1723 <jayesh.prajapati2024@sait.ac.in>
2026-06-21 23:38:57 +0530

    class codes of java

---
+++ oops/Classone.java
@@ -0,0 +1,11 @@
+public class Classone {
+
+static void show (){
+System.out.println("me static hu ") ;
+}
+    public static void main(String[] args) {
+    
+System.out.println("me non static hu bad m chlta hu phle static")  ;
+ show() ;   
+}    
+}
