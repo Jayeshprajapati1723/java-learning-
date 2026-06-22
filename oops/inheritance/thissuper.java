a1799ebfdd192ca702edce43cef18c032bc962b6
Jayeshprajapati1723 <jayesh.prajapati2024@sait.ac.in>
2026-06-22 23:29:53 +0530

    java codes with practices with inheritance

---
+++ oops/inheritance/thissuper.java
@@ -0,0 +1,19 @@
+class aa {
+aa() {
+    System.out.println("hello jayesh from aa") ;
+}
+}
+class bb extends aa{
+bb() {
+    super(); //use for change the constructor of parent class
+    System.out.println("hello jayesh from bb") ;
+}
+}
+
+public class thissuper {
+    public static void main(String[] args) {
+        bb b1 = new bb() ;
+
+    }
+    
+}
