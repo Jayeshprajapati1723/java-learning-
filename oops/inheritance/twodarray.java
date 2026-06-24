8832a6799dd0747a915dcc9ed2d221c223ff60df
Jayeshprajapati1723 <jayesh.prajapati2024@sait.ac.in>
2026-06-24 23:39:14 +0530

    notes of java with codes

--- oops/inheritance/twodarray.java
+++ oops/inheritance/twodarray.java
@@ -4,6 +4,7 @@
 
 public class twodarray {
 public static void main(String[] args) {
-    int tdarr[][] = new int[2][] ;
+    int tdarr[][] = new int[2][3] ;
+    System.out.println(+tdarr[0][1] );
 }    
 }   
