03ce753c2ed609bfe0d1b01bd29f7cbdf6f3aba7
Jayeshprajapati1723 <jayesh.prajapati2024@sait.ac.in>
2026-06-28 22:30:48 +0530

    string concept

---
+++ oops/inheritance/strinjava.java
@@ -0,0 +1,8 @@
+public class strinjava {
+public static void main(String[] args) {
+    String s = "kumar" ; String f = "jay" ;
+    //concetenet the two string 
+    String fn = f + " "+ s ;
+    System.out.println(fn);  
+}    
+}
